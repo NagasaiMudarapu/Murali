81350b9e49c3aefd8339c2426d9720853e6b64e7
NagasaiMudarapu <nagasaimudarapu74@gmail.com>
2020-07-23 17:05:53 +0530

    Trial

---
+++ index.js
@@ -0,0 +1,21 @@
+var ex = require("express");
+var ap = ex();
+
+ap.get('/',function(rq,rs){
+    rs.send("Home Page");
+})
+
+ap.get('/page_1 ',function(rq,rs){
+    rs.send("Page_1");
+})
+
+ap.get('/page_2 ',function(rq,rs){
+    rs.send("Page_2");
+})
+
+var po = process.env.PORT || 3000
+
+ap.listen(po, function(){
+    console.log("Site running on https://localhost:" + po);
+});
+
